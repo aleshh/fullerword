f088644cdf2ecfef144a8118e3653d55ef026a00
Alesh Houdek <mail@alesh.com>
2017-11-29 06:46:32 -0700

    added sample word file

---
+++ src/app/services/sample-words.ts
@@ -0,0 +1,42 @@
+import { Entry } from '../models/Entry';
+
+export const sampleWords: Entry[] = [
+  {
+    text: "elan",
+    definition: "energy, style, and enthusiasm",
+    tags: [
+      "positive",
+      "personal",
+      "adjective"
+    ],
+    sampleData: true
+  },
+  {
+    text: "mordant",
+    definition: "(especially of humour) having or showing a sharp or critical quality; biting",
+    tags: [
+      "sharp",
+      "adjective",
+      "personal"
+    ],
+    sampleData: true
+  },
+  {
+    text: "claptrap",
+    definition: "absurd or nonsensical talk or ideas",
+    tags: [
+      "zany",
+      "noun",
+      "descriptive"
+    ],
+    sampleData: true
+  },
+  {
+    text: "quizzical",
+    definition: "(of a person's expression or behaviour) indicating mild or amused puzzlement",
+    tags:[
+      "adjective"
+    ],
+    sampleData: true
+  }
+]
