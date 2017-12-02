79fb3121058e746a168727283fe587ca56aeee9f
Alesh Houdek <mail@alesh.com>
2017-12-02 15:23:49 -0700

    refactor and cleanup

--- src/app/components/explore/explore.component.ts
+++ src/app/components/explore/explore.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit, Injectable } from '@angular/core';
 import { DataService } from '../../services/data.service';
 import { UtilitiesService } from '../../services/utilities.service';
 import { Entry } from '../../models/Entry';
-// import { setTimeout } from 'timers';
 
 @Component({
   selector: 'app-explore',
@@ -27,13 +26,11 @@ export class ExploreComponent implements OnInit {
     this.useStarRating = this.dataService.getPreference('useStarRating');
 
     this.changeSort(this.sortBy);
-    scroll(0, 70);
-    console.log('entries: ', this.entries );
+    this.hidePreferences();
   }
 
   toggleStar(entry: Entry) {
     entry.star = !entry.star;
-    console.log(entry.text, ' star: ', entry.star);
   }
 
   changeSort(event): void {
@@ -42,9 +39,6 @@ export class ExploreComponent implements OnInit {
 
     }
     this.sortBy = event;
-    // if (this.sortBy != this.dataService.getPreference('sortWordListBy')) {
-    //   this.dataService.setPreference('sortWordListBy', event);
-    // }
     switch (this.sortBy) {
       case ('newest'):
       this.entries.sort((a:Entry, b:Entry) => {
@@ -70,13 +64,14 @@ export class ExploreComponent implements OnInit {
     }
   }
 
-  addSampleData():void {
+  addSampleData(): void {
     this.dataService.loadSampleData();
-    setTimeout(this.hidePreferences, 0);
+    this.hidePreferences();
   }
 
   private hidePreferences(): void {
-    scroll(0, 70);
+    setTimeout(() => {scroll(0, 70);}, 0);
+    // scroll(0, 70);
   }
 
 }
