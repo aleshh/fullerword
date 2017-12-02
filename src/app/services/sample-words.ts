cc09227e76df19ab458786c1462f8c62e3e18d98
Alesh Houdek <mail@alesh.com>
2017-12-02 15:46:33 -0700

    added more sample words

--- src/app/services/sample-words.ts
+++ src/app/services/sample-words.ts
@@ -66,5 +66,175 @@ export const sampleWords: Entry[] = [
       "sample-data"
     ],
     sampleData: true
+  },
+  {
+    text: "preen",
+    definition: "devote effort to making oneself look attractive and then admire one's appearance",
+    tags:[
+      "verb",
+      "personal",
+      "negative",
+      "appearance",
+      "sample-data"
+    ],
+    sampleData: true
+  },
+  {
+    text: "narcissist",
+    definition: "a person who has an excessive interest in or admiration of themselves",
+    tags:[
+      "noun",
+      "personal",
+      "negative",
+      "sample-data"
+    ],
+    sampleData: true
+  },
+  {
+    text: "profligate",
+    definition: "recklessly extravagant or wasteful in the use of resources",
+    tags:[
+      "adjective",
+      "personal",
+      "negative",
+      "sample-data"
+    ],
+    sampleData: true
+  },
+  {
+    text: "dissolute",
+    definition: "overindulging in sensual pleasures",
+    tags:[
+      "adjective",
+      "negative",
+      "sexual",
+      "personal",
+      "sample-data"
+    ],
+    sampleData: true
+  },
+  {
+    text: "Flub dub",
+    definition: "useless decoration",
+    source: "Abraham Lincoln",
+    tags:[
+      "noun",
+      "negative",
+      "sample-data"
+    ],
+    sampleData: true
+  },
+  {
+    text: "mendacity",
+    definition: "untruthfulness",
+    tags:[
+      "noun",
+      "negative",
+      "sample-data"
+    ],
+    sampleData: true
+  },
+  {
+    text: "compos mentis",
+    definition: "having full control of one's mind",
+    tags:[
+      "latin",
+      "adjective",
+      "positive",
+      "sample-data"
+    ],
+    sampleData: true
+  },
+  {
+    text: "anodyne",
+    definition: "dull",
+    tags:[
+      "adjective",
+      "negative",
+      "sample-data"
+    ],
+    sampleData: true
+  },
+  {
+    text: "quotidian",
+    definition: "everyday, ordinary",
+    tags:[
+      "adjective",
+      "negative",
+      "sample-data"
+    ],
+    sampleData: true
+  },
+  {
+    text: "vituperative",
+    definition: "abusive",
+    tags:[
+      "adjective",
+      "negative",
+      "sample-data"
+    ],
+    sampleData: true
+  },
+  {
+    text: "prosaic",
+    definition: "commonplace",
+    tags:[
+      "adjective",
+      "negative",
+      "sample-data"
+    ],
+    sampleData: true
+  },
+  {
+    text: "sesquipedalian",
+    definition: "given to or characterized by the use of long words",
+    tags:[
+      "adjective",
+      "meta",
+      "sample-data"
+    ],
+    sampleData: true
+  },
+  {
+    text: "ignominy",
+    definition: "public shame or disgrace",
+    tags:[
+      "noun",
+      "negative",
+      "sample-data"
+    ],
+    sampleData: true
+  },
+  {
+    text: "maudlin",
+    definition: "self-pityingly or tearfully sentimental",
+    tags:[
+      "adjective",
+      "sad",
+      "sample-data"
+    ],
+    sampleData: true
+  },
+  {
+    text: "claptrap",
+    definition: "absurd or nonsensical talk",
+    tags:[
+      "noun",
+      "negative",
+      "sample-data"
+    ],
+    sampleData: true
   }
 ]
+
+// ,
+// {
+//   text: "",
+//   definition: "",
+//   tags:[
+//     "",
+//     "",
+//     "sample-data"
+//   ],
+//   sampleData: true
+// }
